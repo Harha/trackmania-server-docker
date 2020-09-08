a7a1430998ca9ddf65c4cdcdfcd2994ed18d732a
harha <toni_hameenniemi@protonmail.com>
2020-09-08 17:53:21 +0300

    created run_server.sh script for actually starting the server with args

---
+++ build/run_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "MASTERSERVER_LOGIN: ${MASTERSERVER_LOGIN}"
+echo "SERVER_IP: ${SERVER_IP}"
+echo "SERVER_TITLE: ${SERVER_TITLE}"
+echo "SERVER_NAME: ${SERVER_NAME}"
+
+./TrackmaniaServer /nodaemon /nologs \
+  /dedicated_cfg="cfg_server.xml" \
+  /game_settings="MatchSettings/cfg_tracklist.xml" \
+  /bindip="${SERVER_IP}" \
+  /title="${SERVER_TITLE}" \
+  /servername="${SERVER_NAME}"
