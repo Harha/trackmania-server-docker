97de8513aaa10d03458b5de62379cb216ae47417
Toni <toni_hameenniemi@protonmail.com>
2020-09-08 23:27:30 +0300

    remove /forceip & /bindip args for now

--- build/run_server.sh
+++ build/run_server.sh
@@ -11,7 +11,5 @@ echo "SERVER_NAME: ${SERVER_NAME}"
 ./TrackmaniaServer /nodaemon /nologs \
   /dedicated_cfg="cfg_server.xml" \
   /game_settings="MatchSettings/cfg_tracklist.xml" \
-  /bindip="${SERVER_IP}:${SERVER_PORT}" \
-  /forceip="${SERVER_IP}:${SERVER_PORT}" \
   /title="${SERVER_TITLE}" \
   /servername="${SERVER_NAME}"
